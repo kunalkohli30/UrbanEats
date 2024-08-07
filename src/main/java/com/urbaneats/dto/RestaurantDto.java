bd252df44cc0377869c63832abdf5fbfca4cb787
Kunal Kohli <kunalkohli30@gmail.com>
2024-08-08 04:27:07 +0530

    git init

---
+++ src/main/java/com/urbaneats/dto/RestaurantDto.java
@@ -0,0 +1,22 @@
+package com.urbaneats.dto;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private List<String> images;
+
+    private String description;
+}
---
+++ src/test/java/com/urbaneats/UrbanEatsDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbaneats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanEatsDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
